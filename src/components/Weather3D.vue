1e53b9878042f720c2d1af0a09bb8f1b210e4b01
suzuru0611 <120362718+suzuru0611@users.noreply.github.com>
2023-03-23 20:33:07 +0800

    03/23

--- src/components/Weather3D.vue
+++ src/components/Weather3D.vue
@@ -1,44 +1,32 @@
 <template>
   <div>
-    <h1>天氣預報</h1>
-    <form @submit.prevent="getWeather">
-      <input type="text" v-model="city" placeholder="請輸入城市名稱" />
-      <button type="submit">查詢</button>
-    </form>
     <div v-if="error">{{ error }}</div>
-    <div v-if="loading">載入中...</div>
-    <div v-if="weather">
-      <h2>{{ weather.name }}</h2>
-      <div>{{ weather.weather[0].description }}</div>
-      <div>溫度：{{ weather.main.temp }} 度</div>
-      <div>體感溫度：{{ weather.main.feels_like }} 度</div>
-      <div>最高溫度：{{ weather.main.temp_max }} 度</div>
-      <div>最低溫度：{{ weather.main.temp_min }} 度</div>
+    <div v-else-if="loading">加載中...</div>
+    <div v-else>
+      <div>城市：{{ weather.name }}</div>
+      <div>溫度：{{ weather.main.temp }}°C</div>
+      <div>天氣：{{ weather.weather[0].description }}</div>
     </div>
   </div>
 </template>
+
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import WeatherService from "../services/WeatherService";
 
-const city = ref("");
 const weather = ref(null);
-const error = ref(null);
 const loading = ref(false);
+const error = ref(null);
 
-const getWeather = async () => {
+onMounted(async () => {
   loading.value = true;
   try {
-    const response = await WeatherService.getWeatherByCityName(city.value);
-    weather.value = response.data;
-    error.value = null;
-  } catch (error) {
-    weather.value = null;
-    error.value = "無法取得天氣資料";
+    const data = await WeatherService.getWeather("Taipei");
+    weather.value = data;
+  } catch (err) {
+    error.value = "獲取天氣資料失敗";
   } finally {
     loading.value = false;
   }
-};
+});
 </script>
-
-<style scoped></style>
--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,14 +1,16 @@
-import axios from "axios";
+const BASE_URL =
+  "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063?Authorization=CWB-B11B778A-92F5-43CD-BBF4-D97FBBDE608D";
+const API_KEY = "CWB-B11B778A-92F5-43CD-BBF4-D97FBBDE608D";
 
-const API_KEY = "YOUR_API_KEY";
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
-
-const WeatherService = {
-  getWeatherByCityName: async (cityName) => {
+export default {
+  async getWeather(cityName) {
     const url = `${BASE_URL}?q=${cityName}&appid=${API_KEY}&units=metric`;
-    const response = await axios.get(url);
-    return response;
+    const response = await fetch(url);
+    const data = await response.json();
+    if (response.ok) {
+      return data;
+    } else {
+      throw new Error(data.message);
+    }
   },
 };
-
-export default WeatherService;
