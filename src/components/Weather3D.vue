a1f26e8bc9275236ac4526aacbbd36e604c1081f
suzuru0611 <120362718+suzuru0611@users.noreply.github.com>
2023-03-25 16:10:57 +0800

    03/25

--- src/components/Weather3D.vue
+++ src/components/Weather3D.vue
@@ -4,9 +4,11 @@
     <div v-else-if="loading">加載中...</div>
     <div v-else>
       <div>城市：{{ city }}</div>
-      <div>溫度：{{ temperature }}°C</div>
+      <div>9A.M溫度：{{ temperature9am }}°C</div>
+      <div>6P.M溫度：{{ temperature6pm }}°C</div>
       <div>天氣狀態：{{ description }}</div>
-      <div>降雨機率：{{ chanceOfRain }}%</div>
+      <div>9A.M降雨機率：{{ chanceOfRain9am }}</div>
+      <div>6P.M降雨機率：{{ chanceOfRain6pm }}</div>
       <div>更新時間：{{ updateTime }}</div>
     </div>
   </div>
@@ -21,9 +23,11 @@ const LOCATION_NAME = "大安區";
 const LOCATION_CODE = "63000080";
 
 const city = ref(null);
-const temperature = ref(null);
+const temperature9am = ref(null);
+const temperature6pm = ref(null);
 const description = ref(null);
-const chanceOfRain = ref(null);
+const chanceOfRain9am = ref(null);
+const chanceOfRain6pm = ref(null);
 const updateTime = ref(null);
 
 const loading = ref(false);
@@ -34,13 +38,18 @@ const fetchData = async () => {
 
   try {
     const res = await axios.get(
-      `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063?Authorization=${API_KEY}&locationName=${LOCATION_NAME}&elementName=Wx,T,PoP12h`
+      `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-061?Authorization=${API_KEY}&locationName=${LOCATION_NAME}&elementName=Wx,T,PoP12h`
     );
     const data = res.data.records.locations[0].location[0];
     city.value = data.locationName;
-    temperature.value = data.weatherElement[1].time[0].elementValue[0].value;
+    chanceOfRain9am.value =
+      data.weatherElement[1].time[7].elementValue[0].value;
+    chanceOfRain6pm.value =
+      data.weatherElement[1].time[10].elementValue[0].value;
+    temperature9am.value = data.weatherElement[2].time[7].elementValue[0].value;
+    temperature6pm.value =
+      data.weatherElement[2].time[10].elementValue[0].value;
     description.value = data.weatherElement[0].time[0].elementValue[0].value;
-    chanceOfRain.value = data.weatherElement[2].time[0].elementValue[0].value;
     updateTime.value = data.weatherElement[0].time[0].startTime;
   } catch (err) {
     error.value = "獲取天氣資料失敗";
