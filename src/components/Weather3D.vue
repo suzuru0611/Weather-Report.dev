3b86b5e3a9a806ced9d7ce0baf1da4f8500f89ed
suzuru0611 <suzuru0611@gmail.com>
2023-04-11 16:30:31 +0800

    04/11

--- src/components/Weather3D.vue
+++ src/components/Weather3D.vue
@@ -11,8 +11,8 @@
         </div>
       </Location>
       <inner-box class="flex h-[85vh] w-full flex-row">
-        <bubble class="w-4/6 bg-rose-600"> </bubble>
-        <information class="w-2/6 flex-col">
+        <bubble class="w-full bg-rose-600 xl:w-4/6"> </bubble>
+        <information class="hidden w-2/6 flex-col xl:flex">
           <div
             class="flex h-3/6 flex-row items-center justify-center border border-indigo-600"
           >
@@ -54,6 +54,47 @@
           </div>
         </information>
       </inner-box>
+      <information class="flex w-full flex-col xl:hidden">
+        <div
+          class="flex h-3/6 flex-row items-center justify-center border border-indigo-600"
+        >
+          <video
+            class="h-full w-screen"
+            src="../assets/rain.webm"
+            autoplay
+            loop
+            muted
+          ></video>
+        </div>
+        <div
+          class="flex h-2/6 flex-col items-center justify-center border border-indigo-600"
+        >
+          <div
+            class="float-left text-2xl font-extrabold text-indigo-600 xl:text-4xl"
+          >
+            明日預報
+          </div>
+          <div class="flex flex-row items-center font-mono text-indigo-600">
+            <div class="pr-3 text-xl xl:text-4xl">9A.M-</div>
+            <div class="text-base xl:text-xl">
+              溫度：{{ temperature9am }}°C,天氣狀態：{{ description9am }}
+            </div>
+          </div>
+          <div class="flex flex-row items-center font-mono text-indigo-600">
+            <div class="pr-3 text-xl xl:text-4xl">6P.M-</div>
+            <div class="text-base xl:text-4xl">
+              溫度：{{ temperature6pm }}°C,天氣狀態：{{ description6pm }}
+            </div>
+          </div>
+        </div>
+        <div
+          class="flex h-1/6 flex-row items-center justify-center border border-indigo-600 text-indigo-600"
+        >
+          <div class="text-xl font-extrabold xl:text-4xl">
+            降雨機率：{{ chanceOfRain }}%
+          </div>
+        </div>
+      </information>
       <UpdateTime class="flex h-[8bh] flex-row xl:h-[5vh]">
         <div
           class="flex w-1/12 justify-center bg-indigo-600 pt-2 pb-2 font-mono text-sm text-white"
