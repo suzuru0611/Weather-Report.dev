825bb88ef83cd66e3901d71f3db9b5498831e9f1
suzuru0611 <suzuru0611@gmail.com>
2023-04-16 02:23:37 +0800

    04/16

--- src/components/Weather3D.vue
+++ src/components/Weather3D.vue
@@ -2,20 +2,45 @@
   <div class="h-screen">
     <div v-if="error">{{ error }}</div>
     <div v-else-if="loading">加載中...</div>
-    <div v-else class="flex flex-col">
-      <Location class="h-[8vh] border border-indigo-600 xl:h-[10vh]">
+    <div v-else class="flex flex-col border border-indigo-600 p-5 xl:h-[100vh]">
+      <Location
+        class="relative flex h-[10vh] flex-row border border-indigo-600 xl:h-[10vh]"
+      >
+        <div class="w-[5rem]">
+          <img src="../assets/logo.png" alt="logo" />
+        </div>
         <div
-          class="flex flex-row items-center font-mono text-3xl text-indigo-600"
+          class="flex h-full w-[10rem] items-center justify-center bg-indigo-600 font-mono text-3xl text-indigo-600"
         >
-          {{ city }}
+          <P class="text-white">
+            {{ city }}
+          </P>
+        </div>
+        <div
+          class="flex w-[5rem] items-center justify-center border-r border-indigo-600"
+        >
+          <div class="text-indigo-600">time</div>
+        </div>
+        <div class="flex w-[10rem] items-center justify-center">
+          <div class="text-indigo-600">temperature</div>
+        </div>
+        <div
+          class="absolute right-0 flex h-full w-[10rem] flex-row items-center justify-center border-l border-indigo-600"
+        >
+          <div class="flex text-2xl font-extrabold text-indigo-600">
+            其餘作品
+          </div>
         </div>
       </Location>
-      <inner-box class="flex h-[85vh] w-full flex-row">
+      <inner-box class="flex h-[80vh] w-full flex-row">
         <bubble class="w-full bg-rose-600 xl:w-4/6"> </bubble>
         <information class="hidden w-2/6 flex-col xl:flex">
           <div
-            class="flex h-3/6 flex-row items-center justify-center border border-indigo-600"
+            class="relative flex h-3/6 flex-row items-center justify-center border-x border-indigo-600"
           >
+            <div class="absolute top-5 left-5">
+              <p class="font-mono text-base text-indigo-600 xl:text-4xl">123</p>
+            </div>
             <video
               class="h-full w-screen"
               src="../assets/rain.webm"
@@ -28,7 +53,7 @@
             class="flex h-2/6 flex-col items-center justify-center border border-indigo-600"
           >
             <div
-              class="float-left text-2xl font-extrabold text-indigo-600 xl:text-4xl"
+              class="xt-2xl fontte-extrabold float-left text-indigo-600 xl:text-4xl"
             >
               明日預報
             </div>
@@ -40,7 +65,7 @@
             </div>
             <div class="flex flex-row items-center font-mono text-indigo-600">
               <div class="pr-3 text-xl xl:text-4xl">6P.M-</div>
-              <div class="text-base xl:text-4xl">
+              <div class="text-base xl:text-xl">
                 溫度：{{ temperature6pm }}°C,天氣狀態：{{ description6pm }}
               </div>
             </div>
@@ -88,7 +113,7 @@
           </div>
         </div>
         <div
-          class="flex h-1/6 flex-row items-center justify-center border border-indigo-600 text-indigo-600"
+          class="flex h-1/6 flex-row items-center justify-center border-indigo-600 text-indigo-600"
         >
           <div class="text-xl font-extrabold xl:text-4xl">
             降雨機率：{{ chanceOfRain }}%
@@ -97,12 +122,12 @@
       </information>
       <UpdateTime class="flex h-[8bh] flex-row xl:h-[5vh]">
         <div
-          class="flex w-1/12 justify-center bg-indigo-600 pt-2 pb-2 font-mono text-sm text-white"
+          class="flex w-2/12 justify-center bg-indigo-600 pt-2 pb-2 font-mono text-sm text-white xl:w-1/12"
         >
           更新時間
         </div>
         <div
-          class="flex w-11/12 justify-center border border-indigo-600 pt-2 pb-2 font-mono text-sm text-indigo-600"
+          class="flex w-10/12 justify-center border border-indigo-600 pt-2 pb-2 font-mono text-sm text-indigo-600 xl:w-11/12"
         >
           {{ updateTime }}
         </div>
